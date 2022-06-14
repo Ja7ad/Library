6125c6a7fa85173c3479d591bd06ace857948fa6
Javad <ja7ad@live.com>
2022-06-14 13:39:22 +0430

    add: env user mongo uri

--- server/main.go
+++ server/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/Ja7ad/library/server/book/db"
-	"github.com/Ja7ad/library/server/book/global"
+	bookDB "github.com/Ja7ad/library/server/db"
+	"github.com/Ja7ad/library/server/global"
+	"github.com/Ja7ad/library/server/transport/grpc"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -12,18 +13,23 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable")
+	bookURI := os.Getenv("MONGO_BOOK_URI")
+	if bookURI == "" {
+		log.Fatal("You must set your 'MONGO_BOOK_URI' environmental variable")
 	}
 
-	trans, err := db.NewMongo(uri)
+	userURI := os.Getenv("MONGO_USER_URI")
+	if userURI == "" {
+		log.Fatal("You must set your 'MONGO_USER_URI' environmental variable")
+	}
+
+	trans, err := bookDB.NewMongo(bookURI)
 	if err != nil {
 		log.Fatal(err)
 	}
-	global.Client = trans
+	global.BookClient = trans
 
-	if err := InitServer("localhost", "3345"); err != nil {
+	if err := grpc.InitServer("localhost", "3345"); err != nil {
 		log.Fatal(err)
 	}
 }

709e8b5632ff8430eb9c422a6241e33b7f44a8a6
Javad <ja7ad@live.com>
2022-06-14 12:16:21 +0430

    add: book logic

---
+++ server/book/models/publisher.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"context"
+	"github.com/Ja7ad/transaction/server/book/global"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var colPub = global.Client.GetDatabase("library").Collection("publisher")
+
+type Publisher struct {
+	Id   primitive.ObjectID `bson:"_id" json:"id"`
+	Name string             `bson:"name" json:"name"`
+}
+
+func (b *Publisher) Insert(ctx context.Context) error {
+	if _, err := colPub.InsertOne(ctx, b); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetPublisherByName(ctx context.Context, publisherName string) (*Publisher, error) {
+	publisher := &Publisher{}
+	if err := colPub.FindOne(ctx, bson.M{"name": publisherName}).Decode(publisher); err != nil {
+		return nil, err
+	}
+	return publisher, nil
+}
---
+++ server/transport.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Ja7ad/transaction/proto/protoModel/library"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func InitServer(addr, port string) error {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", addr, port))
+	if err != nil {
+		return err
+	} else {
+		log.Println("server ran on :3345")
+	}
+	srv := grpc.NewServer()
+	library.RegisterLibraryServiceServer(srv, &LibraryServer{})
+
+	if err := srv.Serve(listener); err != nil {
+		return err
+	}
+	return nil
+}

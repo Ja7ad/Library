1eb00ba36f6bb1c219cf4ee74c752175da5488b0
Javad <ja7ad@live.com>
2022-08-18 10:17:41 +0430

    remove: proto generator

--- protoGen.sh
+++
@@ -1,1 +0,0 @@
-protoc --proto_path=proto --go-grpc_out=proto/protoModel --go_out=proto/protoModel --grpc-gateway_out=logtostderr=true:proto/protoModel --openapiv2_out api/swagger --openapiv2_opt logtostderr=true --openapiv2_opt generate_unbound_methods=true proto/library.proto
